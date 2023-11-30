7a6d32c99d0d559f394ccd4400b08a962ebeef11
wass <ouassim.krifa@cleany.fr>
2023-11-30 09:57:12 +0100

    :recycle: DDD refacto

--- src/application/timecard-computation/compute-timecard-for-employee.ts
+++ src/application/timecard-computation/compute-timecard-for-employee.ts
@@ -6,9 +6,10 @@ import {EmploymentContract} from '@domain/models/employment-contract-management/
 import {Leave} from '@domain/models/leave-recording/leave/leave';
 import {LocalDateRange} from '@domain/models/local-date-range';
 import {Shift} from '@domain/models/mission-delivery/shift/shift';
-import {TimeCard} from '@domain/models/time-card-computation/time-card/TimeCard';
+import {TimeCard, WorkingPeriodTimecard} from '@domain/models/time-card-computation/time-card/TimeCard';
+import {WorkedHoursRate} from '@domain/models/time-card-computation/time-card/WorkedHoursRate';
 import {WorkingPeriod} from '@domain/models/time-card-computation/working-period/WorkingPeriod';
-import {Duration} from '@js-joda/core';
+import {Duration, TemporalUnit} from '@js-joda/core';
 import {TimecardComputationError} from '@shared/error/TimecardComputationError';
 import * as E from 'fp-ts/Either';
 import {pipe} from 'fp-ts/function';
@@ -17,20 +18,83 @@ import {List, Map} from 'immutable';
 const computeTotalHoursByWorkingPeriod = (groupedShifts: Map<WorkingPeriod, List<Shift>>) =>
   E.right(groupedShifts.map(gs => gs.reduce((acc, sh) => acc.plus(sh.duration), Duration.ZERO)));
 
-const computeTotalAdditionalHours = (
+const computeTotalHours = (shifts: List<Shift>) => (timecard: WorkingPeriodTimecard) =>
+  timecard.workedHours.set(
+    'TotalWeekly',
+    shifts.reduce((acc, sh) => acc.plus(sh.duration), Duration.ZERO)
+  );
+
+const computeAdditionalHours = (
+  timecard: WorkingPeriodTimecard,
+  contract: EmploymentContract
+  // contracts: List<EmploymentContract>,
+  // totalWeekly: Map<WorkingPeriod, Duration>
+) => {
+  if (contract?.isFullTime()) return timecard;
+
+  return timecard.register(
+    'TotalComplementary',
+    Duration.ofMinutes(
+      Math.max(0, timecard.workedHours.get('TotalWeekly').toMinutes() - contract.weeklyTotalWorkedHours.toMinutes())
+    )
+  );
+};
+
+const computeTotalSupplementaryHours = (
   contracts: List<EmploymentContract>,
   totalWeekly: Map<WorkingPeriod, Duration>
 ) => {
-  const totalAdditionalHoursMap = totalWeekly.reduce((acc, total, workingPeriod) => {
+  const totalSupplementaryHoursMap = totalWeekly.reduce((acc, total, workingPeriod) => {
     const contract = contracts.find(c => c.id === workingPeriod.employmentContractId);
-    return !contract ? acc : acc.set(workingPeriod, total.minus(contract.weeklyTotalWorkedHours));
+    if (!contract?.isFullTime()) return acc.set(workingPeriod, Duration.ZERO);
+    return !contract
+      ? acc
+      : acc.set(
+          workingPeriod,
+          Duration.ofMinutes(Math.max(0, total.toMinutes() - contract.weeklyTotalWorkedHours.toMinutes()))
+        );
   }, Map<WorkingPeriod, Duration>());
   return pipe(
-    totalAdditionalHoursMap,
+    totalSupplementaryHoursMap,
     E.fromPredicate(
-      () => totalAdditionalHoursMap.size === totalWeekly.size,
+      () => totalSupplementaryHoursMap.size === totalWeekly.size,
       () => new TimecardComputationError('Missing contract')
     )
+  );
+};
+const findContract = (contracts: List<EmploymentContract>, workingPeriod: WorkingPeriod) =>
+  pipe(
+    contracts.find(c => c.id === workingPeriod.employmentContractId),
+    E.fromNullable(() => new TimecardComputationError('Missing contract'))
+  );
+
+const initializeWorkingPeriodTimecard = (workingPeriod: WorkingPeriod) =>
+  WorkingPeriodTimecard.build({
+    contractId: workingPeriod.employmentContractId,
+    employeeId: workingPeriod.employeeId,
+    workingPeriod: workingPeriod,
+    workedHours: Map<WorkedHoursRate, Duration>(),
+  });
+// write a function that takes a working period, shifts and leaves and returns a working period timecard
+export const computeWorkingPeriodTimecard: (
+  workingPeriod: WorkingPeriod,
+  shifts: List<Shift>,
+  leaves: List<Leave>,
+  contract: EmploymentContract
+) => E.Either<TimecardComputationError, WorkingPeriodTimecard> = (workingPeriod, shifts, leaves, contracts) => {
+  const wpTimecard = WorkingPeriodTimecard.build({
+    contractId: workingPeriod.employmentContractId,
+    employeeId: workingPeriod.employeeId,
+    workingPeriod: workingPeriod,
+    workedHours: Map<WorkedHoursRate, Duration>(),
+  });
+  pipe(
+    workingPeriod,
+    initializeWorkingPeriodTimecard,
+    computeTotalHours(shifts),
+    computeAdditionalHours(contracts, totalWeekly)
+    // computeTotalHours({workingPeriod, shifts, contract})),
+    // E.map(tc => tc )
   );
 };
 
@@ -47,14 +111,29 @@ export const computeTimecardForEmployee: (
     E.bindW('groupedShifts', ({workingPeriods}) => groupShiftsByWorkingPeriods(shifts, workingPeriods)),
     E.bindW('groupedLeaves', () => E.right(Map<WorkingPeriod, List<Leave>>())),
     E.bindW('totalWeekly', ({groupedShifts}) => computeTotalHoursByWorkingPeriod(groupedShifts)),
-    E.bindW('totalAdditionalHours', ({totalWeekly}) => computeTotalAdditionalHours(contracts, totalWeekly)),
-    E.map(({workingPeriods, groupedShifts, totalWeekly, totalAdditionalHours}) => {
+    E.bindW('totalAdditionalHours', ({totalWeekly}) => computeAdditionalHours(contracts, totalWeekly)),
+    E.bindW('totalSupplementaryHours', ({totalWeekly}) => computeTotalSupplementaryHours(contracts, totalWeekly)),
+    E.map(({workingPeriods, groupedShifts, totalWeekly, totalAdditionalHours, totalSupplementaryHours}) => {
       console.log(
         totalAdditionalHours
           .map(
             (total, wp) =>
-              `${wp.period.toFormattedString()} T: ${totalWeekly.get(wp)?.toHours()}h AH: ${total.toHours()}h${total.toHours() %60}`
-          ).valueSeq()
+              `${wp.period.toFormattedString()} ${contracts
+                .find(c => c.id === wp.employmentContractId)
+                ?.weeklyTotalWorkedHours.toString()}
+              TotalHours: ${totalWeekly.get(wp)?.toHours()}h${
+                (totalWeekly.get(wp)?.toMinutes() || 0) % 60 > 0
+                  ? `${(totalWeekly.get(wp)?.toMinutes() || 0) % 60} `
+                  : ''
+              }
+              AdditionalHours: ${total.toHours()}h${total.toMinutes() % 60 > 0 ? `${total.toMinutes() % 60}` : ''}
+              SupplementaryHours: ${totalSupplementaryHours.get(wp)?.toHours()}h${
+                (totalSupplementaryHours.get(wp)?.toMinutes() || 0) % 60 > 0
+                  ? `${(totalSupplementaryHours.get(wp)?.toMinutes() || 0) % 60}`
+                  : ''
+              }`
+          )
+          .valueSeq()
           .toArray()
           .join('\n')
       );
@@ -65,6 +144,7 @@ export const computeTimecardForEmployee: (
         groupedShifts,
         totalWeekly,
         totalAdditionalHours,
+        totalSupplementaryHours,
       };
     })
   );
--- src/domain/models/time-card-computation/time-card/TimeCard.ts
+++ src/domain/models/time-card-computation/time-card/TimeCard.ts
@@ -1,14 +1,73 @@
-import {Map} from 'immutable';
-import {Duration} from '@js-joda/core';
+import {EmployeeId} from '@domain/models/employee-registration/employee/employee-id';
+import {EmploymentContractId} from '@domain/models/employment-contract-management/employment-contract/employment-contract-id';
+import {WorkingPeriod} from '@domain/models/time-card-computation/working-period/WorkingPeriod';
 
 import {WorkingPeriodId} from '@domain/models/time-card-computation/working-period/WorkingPeriodId';
-import {EmploymentContractId} from '@domain/models/employment-contract-management/employment-contract/employment-contract-id';
-import {EmployeeId} from '@domain/models/employee-registration/employee/employee-id';
+import {Duration} from '@js-joda/core';
+import {List, Map, ValueObject, Record} from 'immutable';
 import {WorkedHoursRate} from './WorkedHoursRate';
 
-export type TimeCard = {
-  cleanerId: EmployeeId;
+type WorkingPeriodTimecardId = string;
+
+export interface WPTimecard {
+  id: WorkingPeriodTimecardId;
+  employeeId: EmployeeId;
   contractId: EmploymentContractId;
-  workingPeriodId: WorkingPeriodId;
-  workedHours: Map<WorkedHoursRate, Duration>;
-};
+  workingPeriod: WorkingPeriod;
+  workedHours: Record<{[K in WorkedHoursRate]: Duration}>;
+}
+
+export class WorkingPeriodTimecard implements ValueObject, WPTimecard {
+  private static count = 0;
+  public static build(params: {
+    employeeId: EmployeeId;
+    contractId: EmploymentContractId;
+    workingPeriod: WorkingPeriod;
+    workedHours: Record<{[K in WorkedHoursRate]: Duration}>;
+  }) {
+    return new WorkingPeriodTimecard(
+      `${WorkingPeriodTimecard.count++}`,
+      params.employeeId,
+      params.contractId,
+      params.workingPeriod,
+      params.workedHours
+    );
+  }
+
+  private readonly _vo: ValueObject;
+
+  private constructor(
+    public readonly id: WorkingPeriodTimecardId,
+    public readonly employeeId: EmployeeId,
+    public readonly contractId: EmploymentContractId,
+    public readonly workingPeriod: WorkingPeriod,
+    public readonly workedHours: Record<{[K in WorkedHoursRate]: Duration}>
+  ) {
+    this._vo = Map<string, ValueObject | string | number | boolean>()
+      .set('id', this.id)
+      .set('employeeId', this.employeeId)
+      .set('contractId', this.contractId)
+      .set('workingPeriod', this.workingPeriod)
+      .set('workedHours', this.workedHours);
+  }
+
+  equals(other: unknown): boolean {
+    return this._vo.equals((other as WorkingPeriodTimecard)?._vo);
+  }
+
+  hashCode(): number {
+    return this._vo.hashCode();
+  }
+
+  register(workedHoursRate: WorkedHoursRate, duration: Duration): WorkingPeriodTimecard {
+    return new WorkingPeriodTimecard(
+      this.id,
+      this.employeeId,
+      this.contractId,
+      this.workingPeriod,
+      this.workedHours.set(workedHoursRate, duration)
+    );
+  }
+}
+
+export type PeriodTimeCard = List<WorkingPeriodTimecard>;
