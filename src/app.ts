bb64393a2f6b87de3fe08211ecfc7c8f1f779c6b
wass <ouassim.krifa@cleany.fr>
2024-07-04 14:29:02 +0200

    🚧 try to fix cors

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://manager-app-wfq6.onrender.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
