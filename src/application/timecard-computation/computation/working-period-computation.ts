d1abe5261db65472d6983ed284915d24905189e6
wass <ouassim.krifa@cleany.fr>
2025-03-05 16:34:05 +0100

    🐛 Fix a bug with split into working period

--- src/application/timecard-computation/computation/working-period-computation.ts
+++ src/application/timecard-computation/computation/working-period-computation.ts
@@ -9,16 +9,19 @@ import { WorkingPeriod } from '../../../domain/models/time-card-computation/work
 import { TimecardComputationError } from '../../../~shared/error/TimecardComputationError';
 
 export const throwIfNoContract = <T>(list: List<T>) =>
-  list.isEmpty() ? E.left(new TimecardComputationError('No contract matches this period 0')) : E.right(list);
+  list.isEmpty()
+    ? E.left(new TimecardComputationError('No contract matches this period 0'))
+    : E.right(list);
 
-export const filterContractsForPeriod = (period: LocalDateRange) => (contracts: List<EmploymentContract>) =>
-  contracts.filter(contract => contract.period(period.end).overlaps(period));
+export const filterContractsForPeriod =
+  (period: LocalDateRange) => (contracts: List<EmploymentContract>) =>
+    contracts.filter(contract => contract.period(period.end).overlaps(period));
 
 const computeWorkingPeriods = (period: LocalDateRange) => (contracts: List<EmploymentContract>) =>
   pipe(
     contracts,
     E.fromPredicate(
-      crts => !crts.isEmpty(),
+      crts => true,
       () => new TimecardComputationError('No contract matches this period 1')
     ),
     E.map(crts =>
@@ -38,10 +41,15 @@ const computeWorkingPeriods = (period: LocalDateRange) => (contracts: List<Emplo
     )
   );
 
-export const splitPeriodIntoWorkingPeriods = (contracts: List<EmploymentContract>, period: LocalDateRange) =>
-  pipe(contracts, computeWorkingPeriods(period));
+export const splitPeriodIntoWorkingPeriods = (
+  contracts: List<EmploymentContract>,
+  period: LocalDateRange
+) => pipe(contracts, computeWorkingPeriods(period));
 
-export const groupShiftsByWorkingPeriods = (shifts: List<Shift>, workingPeriods: List<WorkingPeriod>) =>
+export const groupShiftsByWorkingPeriods = (
+  shifts: List<Shift>,
+  workingPeriods: List<WorkingPeriod>
+) =>
   pipe(workingPeriods, wp =>
     E.right(
       wp.reduce(
@@ -57,7 +65,10 @@ export const groupShiftsByWorkingPeriods = (shifts: List<Shift>, workingPeriods:
     )
   );
 
-export const groupLeavesByWorkingPeriods = (leaves: List<Leave>, workingPeriods: List<WorkingPeriod>) =>
+export const groupLeavesByWorkingPeriods = (
+  leaves: List<Leave>,
+  workingPeriods: List<WorkingPeriod>
+) =>
   pipe(workingPeriods, wp =>
     E.right(
       wp.reduce(
